fa6a437f6a40751c8a396801f2c70513cac643a1
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2023-08-08 21:58:37 -0400

    firebase init

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'country-data-81ef9',
+    appId: '1:865642808722:web:cd2ea139f73f4babb52d4b',
+    storageBucket: 'country-data-81ef9.appspot.com',
+    apiKey: 'AIzaSyC5EMF2UcfURs2aTxaU4pQrHHa7KpC9vxw',
+    authDomain: 'country-data-81ef9.firebaseapp.com',
+    messagingSenderId: '865642808722',
+    measurementId: 'G-LT0HQRY87J',
+  },
+};
